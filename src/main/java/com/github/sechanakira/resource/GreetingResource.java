d636dc2143330114ac9cfa57339461456feebc8a
Shingirai Chanakira <sechanakira@yahoo.com>
2020-04-13 15:55:35 +0200

    Added a REST endpoint

--- src/main/java/com/github/sechanakira/resource/GreetingResource.java
+++ src/main/java/com/github/sechanakira/resource/GreetingResource.java
@@ -25,7 +25,7 @@ public class GreetingResource {
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return "hello";
+    public String mhoro() {
+        return "mhoro";
     }
 }
